b4a36809b02a0e12069486155b67e5dcdbd5b504
Venjora <ravenjohn3003@gmail.com>
2021-10-13 13:45:31 -0700

    edit and adding photo container

--- public/js/main.js
+++ public/js/main.js
@@ -37,3 +37,18 @@ function showUser(str) {
     xmlhttp.send();
   }
 }
+
+window.addEventListener("load", function () {
+  document
+    .querySelector('input[type="file"]')
+    .addEventListener("change", function () {
+      if (this.files && this.files[0]) {
+        var img = document.querySelector("#profileImg");
+        img.onload = () => {
+          URL.revokeObjectURL(img.src); // no longer needed, free memory
+        };
+
+        img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+      }
+    });
+});
