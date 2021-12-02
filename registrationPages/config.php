b184906b30948afdb8f254e5b5877aa1b071e5f7
Venjora <ravenjohn3003@gmail.com>
2021-12-01 20:40:06 -0800

    updated profile

--- registrationPages/config.php
+++ registrationPages/config.php
@@ -3,8 +3,8 @@
 $servername = "127.0.0.1";
 $username = "root";
 $password = "";
-$dbName = "userinformation";
-// $dbName = "cwb_db";
+// $dbName = "userinformation";
+$dbName = "cwb_db";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbName);
